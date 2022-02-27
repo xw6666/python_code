ff820b9ff8a52b663afc3ac9af40163631a79176
xw6666 <1481016131@qq.com>
2022-02-27 19:25:07 +0800

    更新第二章代码

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # python_code
-学习python代码汇总
+学习python代码汇总，学习书籍为黑马程序员的python程序设计现代方法
+
---
+++ charper2/homework1.py
@@ -0,0 +1,11 @@
+# a = eval(input("请输入一个3位以上的整数："))
+# print(a // 100)
+
+
+# a = 93
+# b = a
+#
+# print(id(a), id(b))
+
+
+import turtle as x
