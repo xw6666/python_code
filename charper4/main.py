8863976116ad05efc5d14bbd024a037f3ffd5329
xw6666 <1481016131@qq.com>
2022-03-08 09:25:20 +0800

    更新第四章循环代码练习

---
+++ charper4/main.py
@@ -0,0 +1,65 @@
+#  01_calc
+# num1 = input("num1 = ")
+# num2 = input("num2 = ")
+# sum = eval(num1) + eval(num2)
+# differ = eval(num1) - eval(num2)
+#
+# print("sum = %d, differ = %d" %(sum, differ))
+
+
+# temp = input("temp = ")
+# if eval(temp) > 15:
+#     print("温度适宜")
+# else:
+#     print("气温较低")
+
+
+# n = input("n = ")
+# fact = 1
+# i = 1
+# while i <= eval(n):
+#     fact = fact * i
+#     i = i + 1
+#
+# print(fact)
+
+
+# string = input("")
+# for c in string:
+#     print(c, end = '')
+
+#
+# string = input("")
+# for i in range(0,len(string)):
+#     print(string[i])
+
+
+# n = input("")
+# #
+# # fact = 1
+# #
+# # for i in range(eval(n), 0, -1):
+# #     print("i = %d" %i)
+# #     fact = fact * i
+# #
+# # print(fact)
+
+# 异常处理
+# try:
+#     代码段1
+# except:
+#     代码段二
+
+# try:
+#     a = input("num1 = ")
+#     b = input("num2 = ")
+#     sum = eval(a) + eval(b)
+#     print(sum)
+# except:
+#     print("程序崩溃")
+
+# for i in range(1,10):
+#     for j in range(1,i + 1):
+#         print("%d * %d = %d" %(j,i,i*j), end = '\t')
+#     print("")
+
