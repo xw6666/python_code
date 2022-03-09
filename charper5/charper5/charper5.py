8fda8b91277d19cfab96f298dea2ab88cc877047
xw6666 <1481016131@qq.com>
2022-03-09 23:23:16 +0800

    更新

---
+++ charper5/charper5/charper5.py
@@ -0,0 +1,21 @@
+
+# ½×³Ë
+#n = input("")
+
+#ans = 1
+#for i in range(1,eval(n) + 1):
+#    ans = ans * i
+
+#print("fact = %d" %ans)
+
+
+#n = input("")
+
+#n = n.split(',')
+
+#b = []
+
+#for i in n:
+#    b.append(int(i))
+
+#print(b)
